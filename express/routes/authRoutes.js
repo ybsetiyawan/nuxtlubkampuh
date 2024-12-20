d879b6a43095802fea9ffda572a0a294ae652086
ybsetiyawan <ybsetiyawan@gmail.com>
2024-12-21 06:48:50 +0700

    delete express

--- express/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/authControllers');
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
--- express/routes/setting/menuUserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const MenuUserController = require("../../controllers/setting/menuUserController");
-const verifyToken = require("../../middleware/auth");
-const router = express.Router();
-
-router.post("/", verifyToken, MenuUserController.createMenuUser);
-router.get("/",  MenuUserController.getMenuUserByRoleId);
-
-module.exports = router;
